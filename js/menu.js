c8596c966f68545d327d51d7fd783474a65ad82c
marcelo2932 <95315137+marcelo2932@users.noreply.github.com>
2021-12-13 17:40:08 -0300

    Add files via upload

---
+++ js/menu.js
@@ -0,0 +1,9 @@
+let burguerButton = document.getElementById('burguer-button');
+let menu = document.getElementById('menu');
+console.log(menu);
+
+// burguerButton contiene el span con el icono
+// burguerButton.addEventListener('que evento', 'que deseo hacer');
+burguerButton.addEventListener('touchstart', function() {
+  menu.classList.toggle('active');
+});
